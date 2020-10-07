1357fcef1e175ac78466151cf6c9eb3998932480
Maksym Kozlov <maksym.kozlov@stryker.com>
2020-10-07 14:40:11 +0300

    Commit1

--- src/com/company/c.java
+++ src/com/company/c.java
@@ -5,6 +5,9 @@ import java.util.Arrays;
 public class c {
     public static void main(String[] args) {
         System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
 
 
     }
